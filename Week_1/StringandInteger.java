dffdaa28c8274690a91f45abed08701be4778c07
Altynai <harmoni5413@gmail.com>
2025-04-01 12:50:49 -0400

    printf

---
+++ Week_1/StringandInteger.java
@@ -0,0 +1,30 @@
+
+    import java.util.Scanner;
+
+    public class StringandInteger {
+
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s1=sc.next();
+                int x=sc.nextInt();
+                //Complete this line
+
+                System.out.printf("-%15s-03%d" +s1, x);
+
+                //java 100
+                //cpp 65
+                //python 50
+
+            }
+            System.out.println("================================");
+
+        }
+
+    }
+
+
+
+
+
