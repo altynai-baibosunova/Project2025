2acd260586e183bacaf638041efcaa3466f25850
Altynai <harmoni5413@gmail.com>
2025-04-17 19:51:00 -0400

    math.hypot

--- Java Intensive: Problems/hypotenuse.java
+++ Java Intensive: Problems/hypotenuse.java
@@ -1,26 +1,16 @@
 import java.util.Scanner;
 
-public class Гипотенуза {
+public class hypotenuse {
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
         int a = scanner.nextInt();
         int b = scanner.nextInt();
 
-
-        //for (int i = 0; i <c ; i++) {
             double c = Math.sqrt (a*a+b*b);
-
             System.out.println(c);
             scanner.close();
-
         }
-
-
-
-
-
     }
-//}
+
