d2c4c6e2916538bc8f539f3eba572533937b3ec5
Altynai <harmoni5413@gmail.com>
2025-04-27 22:46:45 -0400

    Factorial

---
+++ Classes/Factorial.java
@@ -0,0 +1,18 @@
+
+
+    public class Factorial {// Recursive function for factorial
+        public static void main (String[] args){
+            System.out.println(factorial(5));
+        }
+        public static int factorial(int n) {
+            // Your code here
+            if (n == 1) {
+                return 1;
+
+            } else {
+                return n * factorial(n - 1);
+            }
+
+        }
+    }
+
