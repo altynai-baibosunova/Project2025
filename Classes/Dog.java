6032cf08fe9f23f62e368ad07f6275bf30a30a78
Altynai <harmoni5413@gmail.com>
2025-04-27 22:47:42 -0400

    Scanner

---
+++ Classes/Dog.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+// Dog class implementation
+    public class Dog {
+        private String name;
+        private String breed;
+
+
+
+        // Constructor
+        public Dog(String name, String breed) {
+            // Initialize attributes
+            this.name=name;
+            this.breed=breed;
+        }
+
+    public static void main(String[] arg){
+        // Dog myDog = new Dog("Mike", "Golden retriver");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        System.out.println("Name of the Dog: "+name);
+
+        String breed = scanner.next();
+        System.out.println("Name of the Breed: "+breed);
+
+    }
+        // Method to make the dog bark
+        public void bark() {
+            // Output the dog's name followed by 'barked'
+            System.out.println(name+breed+" barked");
+        }
+
+    }
+
