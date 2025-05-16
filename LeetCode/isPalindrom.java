cfd30e7b72dfcf45239bdc7d54bb6a51382bd5e8
Altynai <harmoni5413@gmail.com>
2025-05-15 23:07:28 -0400

    isPalindrom

---
+++ LeetCode/isPalindrom.java
@@ -0,0 +1,27 @@
+public class isPalindrom {
+
+    class Solution {
+        public boolean isPalindrome(int x) {
+
+            int temp = x;
+            int length = 0;
+
+            for (int i=temp; i!=0; i/=10){
+                length++
+            }
+            for (int g=0; g>=x; g--){
+
+               }
+                if(temp = x){
+                    return true;
+            }
+
+        }
+
+        public static void main(String[] args) {
+
+            int x=121;
+            System.out.println(isPalindrom);
+        }
+    }
+
