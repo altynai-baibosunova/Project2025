d821447d9757bf690e6ab35cee23ae637933b702
Altynai <harmoni5413@gmail.com>
2025-05-22 10:29:36 -0400

    arraylist6

--- Classes/Remote.java
+++ Classes/Remote.java
@@ -1,26 +1,26 @@
-public class Remote {
-
-    abstract class tv {
-       abstract void turnON();
-        abstract void turnOFF();
-    }
-
-    class OtherChannel extends tv {
-        @Override
-        void turnON() {
-            System.out.println("Turn On TV");
-        }
-        void turnOFF(){
-            System.out.println("Turn Off TV");
-        }
-    }
-
-    public class Main{
-        public void main(String[] args) {
-            tv rem = new OtherChannel();
-            rem.turnON();
-
-        }
-    }
-
-}
+//public class Remote {
+//
+//    abstract class tv {
+//       abstract void turnON();
+//        abstract void turnOFF();
+//    }
+//
+//    class OtherChannel extends tv {
+//        @Override
+//        void turnON() {
+//            System.out.println("Turn On TV");
+//        }
+//        void turnOFF(){
+//            System.out.println("Turn Off TV");
+//        }
+//    }
+//
+//    public class Main{
+//        public void main(String[] args) {
+//            tv rem = new OtherChannel();
+//            rem.turnON();
+//
+//        }
+//    }
+//
+//}
