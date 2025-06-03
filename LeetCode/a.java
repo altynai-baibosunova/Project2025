e6c565fa8bdeba16bbb98e8f547f4eb5c6abd311
Altynai <harmoni5413@gmail.com>
2025-06-02 23:57:31 -0400

    duplicates

---
+++ LeetCode/a.java
@@ -0,0 +1,20 @@
+//public class a {
+//
+//    public int reverse(int x) {
+//        int reversed = 0;
+//
+//        for (int i = 0; i <= x; i++) {
+//            for (int j = x - 1; j >= 0; j--) {
+//                reversed += x;
+//            }
+//        }
+//return reversed;
+//    }
+//    public static void main(String[] args) {
+//        a obj = new a();
+//        int result = obj.reverse();
+//        System.out.println(result);
+//    }
+//}
+//
+
