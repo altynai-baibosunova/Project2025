dcdccd6383c08e6a8dc86b5bed2329a99dac31ec
Altynai <harmoni5413@gmail.com>
2025-07-28 12:23:37 -0400

    reverse

---
+++ Interview/Reverse.java
@@ -0,0 +1,12 @@
+public class Reverse {
+    public static void main(String[] args) {
+        String original = "Hello";
+        String reverse = "";
+
+        for(int i=original.length()-1; i>=0; i--) {
+            reverse += original.charAt(i);
+        }
+            System.out.println("Reversed word "+reverse);
+        }
+    }
+
