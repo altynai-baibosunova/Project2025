af870115807466232a4c9cc6ad2c93ef50b1ba4a
Altynai <harmoni5413@gmail.com>
2025-07-28 12:25:18 -0400

    reverse-StringBuilder

--- Interview/Reverse.java
+++ Interview/Reverse.java
@@ -1,12 +1,20 @@
+import javax.crypto.spec.PSource;
+
 public class Reverse {
     public static void main(String[] args) {
-        String original = "Hello";
-        String reverse = "";
+//        String original = "Hello";
+//        String reverse = "";
+//
+//        for(int i=original.length()-1; i>=0; i--) {
+//            reverse += original.charAt(i);
+//        }
+//            System.out.println("Reversed word "+reverse);
 
-        for(int i=original.length()-1; i>=0; i--) {
-            reverse += original.charAt(i);
-        }
-            System.out.println("Reversed word "+reverse);
-        }
+        String original = "Hello";
+        String reverse = new StringBuilder(original).reverse().toString();
+        System.out.println(reverse);
+     }
+
+
     }
 
