adb594044b8ac2b27c3b903158ddc9660ba307d3
Altynai <harmoni5413@gmail.com>
2025-08-29 13:21:36 -0400

    palindrome

--- LeetCode/A_219_ContainsDuplicate_2.java
+++ LeetCode/A_219_ContainsDuplicate_2.java
@@ -2,7 +2,7 @@ public class A_219_ContainsDuplicate_2 {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
                                            //nums=[1,    2,  3,   1]
         for(int i=0; i<nums.length; i++){ //      [0:1, 1:2, 2:3, 3:1]
-            for(int j=i+1; j<nums.length; i++){ //   [1:2, 1:2, 1:3, 1:1]
+            for(int j=i+1; j<nums.length; j++){ //   [1:2, 1:2, 1:3, 1:1]
                                                    //  0    1    2    3
                 if(nums[i]==nums[j]){
                     if(j-i <=k){
@@ -16,7 +16,7 @@ public class A_219_ContainsDuplicate_2 {
     }
 
     public static void main(String[] args) {
-        int[] nums = {1,2,3,1};
+        int[] nums = {1,2,1,8};
         A_219_ContainsDuplicate_2 obj = new A_219_ContainsDuplicate_2();
         boolean result = obj.containsNearbyDuplicate(nums, 3);
         System.out.println(result);
