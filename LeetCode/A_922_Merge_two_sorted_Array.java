56a573f1e25ff494fef06128f0c7f56093ee79fe
Altynai <harmoni5413@gmail.com>
2025-09-25 11:26:40 -0400

    Leetcode - 922 Merge Sorted Array

---
+++ LeetCode/A_922_Merge_two_sorted_Array.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+public class A_922_Merge_two_sorted_Array {
+            public static int[] sortArrayByParityII(int[] nums) {
+                int i = 0;
+                int j = 1;
+                int n = nums.length;
+
+                while (i < n && j < n) {
+                    while (i < n && nums[i] % 2 == 0) {
+                        i += 2;
+                    }
+                    while (j < n && nums[j] % 2 == 1) {
+                        j += 2;
+                    }
+                    if (i < n && j < n) {
+                        swap(nums, i, j);
+
+                    }
+                }
+                return nums;
+            }
+                private static void swap(int[] nums, int i, int j){
+                   int temp = nums[i];
+                   nums[i] = nums[j];
+                   nums[j] = temp;
+
+            }
+
+
+    public static void main(String[] args) {
+        // Example test
+        int[] nums = {4, 2, 5, 7};
+        int[] result = A_922_Merge_two_sorted_Array.sortArrayByParityII(nums);
+
+        System.out.println("Result from main: " + Arrays.toString(result));
+    }
+
+}
