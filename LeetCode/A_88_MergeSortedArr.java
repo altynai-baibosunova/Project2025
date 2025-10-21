ff93da32c45ead6e20802c95e3cd022ec8bead00
Altynai <harmoni5413@gmail.com>
2025-10-21 09:26:49 -0400

    Leetcode - 88_MergeSortedArr

--- LeetCode/A_88_MergeSortedArr.java
+++ LeetCode/A_88_MergeSortedArr.java
@@ -4,5 +4,25 @@ public class A_88_MergeSortedArr {
         int p1 = 0;
         int p2 = 0;
 
+        while(p1<m && p2<n) {
+            if (nums1[p1] < nums2[p2]) {
+                arr[m + n] = nums1[p1];
+                p1++;
+            }else {
+                arr[m+n] = nums2[p2];
+                p2++;
+            }
+        }
+        while (p1<m){
+            arr[m+n] = nums1[p1];
+            p1++;
+        }
+        while (p2<n){
+            arr[m+n] = nums2[p2];
+            p2++;
+        }
+        for (int i = 0; i < m+n; i++) {
+            nums1[i] = arr[i];
+        }
     }
 }
