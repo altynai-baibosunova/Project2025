ad09c7f9bca0e025961d132379512e13d4ffb640
Altynai <harmoni5413@gmail.com>
2025-10-22 14:56:44 -0400

    Leetcode - 344_ReverseString WithStringBuilder

--- LeetCode/A_344_ReverseString.java
+++ LeetCode/A_344_ReverseString.java
@@ -15,5 +15,16 @@ return s;
     public static void main(String[] args) {
         char[] s = {'h','e','l'};
         System.out.println(reverse(s));
+        System.out.println(reverseWithStringBuilder(s));
+    }
+    public static char[] reverseWithStringBuilder(char[] s){
+        String str = new String(s);
+        String rev = new StringBuilder(str).reverse().toString();
+
+        for (int i = 0; i < s.length; i++) {
+            s[i] = rev.charAt(i);
+
+        }
+        return s;
     }
 }
