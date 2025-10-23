4d97530440b9ffcfdb3be15f879cbd848c16263b
Altynai <harmoni5413@gmail.com>
2025-10-23 17:42:14 -0400

    Leetcode - 26_RemoveDupFromSortedArray + main

--- LeetCode/A_26_RemoveDupFromSortedArray.java
+++ LeetCode/A_26_RemoveDupFromSortedArray.java
@@ -1,5 +1,7 @@
+import java.util.Arrays;
+
 public class A_26_RemoveDupFromSortedArray {
-    public int removeDuplicates(int[] nums) {
+    public static int removeDuplicates(int[] nums) {
 
         int i=0;
         int j=0;
@@ -13,5 +15,10 @@ public class A_26_RemoveDupFromSortedArray {
             j++;
         }
         return i+1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,2,3};
+        System.out.println(removeDuplicates(nums));
     }
 }
