ec4c8714b91434b9cccd76fb7edd40ca61bd3eac
Altynai <harmoni5413@gmail.com>
2025-10-24 22:54:07 -0400

    Leetcode - 14_LongestPrefix + main

--- LeetCode/A_14_LongestPrefix.java
+++ LeetCode/A_14_LongestPrefix.java
@@ -1,13 +1,18 @@
 public class A_14_LongestPrefix {
-    public String longestCommonPrefix(String[] strs) {
+    public static String longestCommonPrefix(String[] strs) {
         if(strs == null || strs.length == 0) return "";
 
         String prefix = strs[0];
         for (int i = 1; i < strs.length; i++) {
-            while (!prefix.startsWith(prefix)){
+            while (!strs[i].startsWith(prefix)){
                 prefix = prefix.substring(0,prefix.length()-1);
             }
         }
         return prefix;
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(strs));
     }
 }
