a69ae8c58319171a9733effc706a31419f0995e5
Altynai <harmoni5413@gmail.com>
2025-10-28 14:09:02 -0400

    Leetcode - 1_Two Sum

--- LeetCode/A_1_Sum_.java
+++ LeetCode/A_1_Sum_.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class A_1_Sum_ {
     public int[] twoSum(int[] nums, int target) {
         int index = 0;
@@ -12,4 +15,19 @@ public class A_1_Sum_ {
         }
         return new int[]{};
     }
+
+    public int[] twoSumHashTable(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int difference = target - nums[i];
+            if(map.containsKey(difference)){
+               return new int[]{nums[i], map.get(difference)};
+            }
+            map.put(nums[i], i);
+
+        }
+     return new int[]{};
+    }
+
 }
+//[2,7,11,15]
