fe82beae81a3f4cd43ff7ae71f98e2827a1a90d5
Altynai <harmoni5413@gmail.com>
2025-10-28 23:25:37 -0400

    Leetcode - 643_MaxAverageSubArray + main

--- LeetCode/A_643_MaxAverageSubArray.java
+++ LeetCode/A_643_MaxAverageSubArray.java
@@ -1,14 +1,19 @@
 public class A_643_MaxAverageSubArray {
-    public double findMaxAverage(int[] nums, int k) {
+    public static double findMaxAverage(int[] nums, int k) {
         double sum = 0;
         for (int i = 0; i < k; i++) {
-            sum = nums[i];
+            sum += nums[i];
         }
         double maxSum = sum;
         for (int i = k; i < nums.length ; i++) {
-            sum = nums[i] - nums[i-k];
+            sum += nums[i] - nums[i-k];
             maxSum = Math.max(sum, maxSum);
         }
-        return maxSum;
+        return maxSum/k;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,12,-5,-6,50,3};
+        System.out.println(findMaxAverage(nums,4));
     }
 }
