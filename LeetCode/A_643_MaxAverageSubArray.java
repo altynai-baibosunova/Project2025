2857b7d9cc6c5bfe07efe6cbc4600af96ed7d44e
Altynai <harmoni5413@gmail.com>
2025-10-28 23:22:06 -0400

    Leetcode - 643_MaxAverageSubArray

---
+++ LeetCode/A_643_MaxAverageSubArray.java
@@ -0,0 +1,14 @@
+public class A_643_MaxAverageSubArray {
+    public double findMaxAverage(int[] nums, int k) {
+        double sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum = nums[i];
+        }
+        double maxSum = sum;
+        for (int i = k; i < nums.length ; i++) {
+            sum = nums[i] - nums[i-k];
+            maxSum = Math.max(sum, maxSum);
+        }
+        return maxSum;
+    }
+}
