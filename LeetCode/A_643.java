22309ce78dffeabad37eb83ba32e5133625981c1
Altynai <harmoni5413@gmail.com>
2025-10-29 22:12:51 -0400

    Leetcode - 643_FindMaxAverage

--- LeetCode/A_643.java
+++ LeetCode/A_643.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class A_643 {
         public double findMaxAverage(int[] nums, int k) {
 
@@ -20,6 +22,21 @@ public class A_643 {
         int[] nums= {1,12,-5,-6,50,3};
         double res = obj.findMaxAverage(nums,4);
         System.out.println(res);
+        System.out.println(findMaxAver(nums,4));
+    }
+
+    public static double findMaxAver(int[] nums, int k) {
+        double sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        double maxSum = sum;
+            for (int j = k; j < nums.length-1; j++) {
+                sum += nums[j] - nums[j-k];
+                maxSum = Math.max(sum, maxSum);
+
+        }
+            return maxSum/k;
     }
 
 }
