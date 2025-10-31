0f49d0c7b936733b6eb46be388ea97d4847a1c54
Altynai <harmoni5413@gmail.com>
2025-10-31 10:29:56 -0400

    Leetcode - 153_FindMaxRotateArray+main

--- LeetCode/A_153_FindMaxRotateArray.java
+++ LeetCode/A_153_FindMaxRotateArray.java
@@ -1,5 +1,5 @@
 public class A_153_FindMaxRotateArray {
-    public int findMin(int[] nums) {
+    public static int findMin(int[] nums) {
         int left=0;
         int right =nums.length-1;
 
@@ -12,5 +12,10 @@ public class A_153_FindMaxRotateArray {
             }
         }
         return nums[left];
+    }
+
+    public static void main(String[] args) {
+        int[] nums= {3,4,5,1,2};
+        System.out.println(findMin(nums));
     }
 }
