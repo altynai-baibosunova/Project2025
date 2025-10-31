c7506e59f8cdff23c353d7d8f3e20d5e6ec72a25
Altynai <harmoni5413@gmail.com>
2025-10-31 12:53:29 -0400

    Leetcode - 283_MoveAll0

--- LeetCode/A_283_MoveAll0.java
+++ LeetCode/A_283_MoveAll0.java
@@ -16,6 +16,23 @@ public class A_283_MoveAll0 {
 
     public static void main(String[] args) {
         int[] nums = {0,1,0,3,12};
-        System.out.println(Arrays.toString(moveZeroes(nums)));
+        System.out.println(Arrays.toString(moveZ(nums)));
+    }
+
+    public static int[] moveZ(int[] nums) {
+        int nonZero = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0){
+                nums[nonZero] = nums[i];
+                nonZero++;
+            }
+
+        }
+        while(nonZero< nums.length){
+            nums[nonZero] = 0;
+            nonZero++;
+        }
+
+        return nums;
     }
 }
