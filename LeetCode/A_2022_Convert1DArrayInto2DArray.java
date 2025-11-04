c94977b94b21f1919af60c91700718c8b8db9017
Altynai <harmoni5413@gmail.com>
2025-11-03 23:04:10 -0500

    Leetcode - 2022

---
+++ LeetCode/A_2022_Convert1DArrayInto2DArray.java
@@ -0,0 +1,23 @@
+//public class A_2022_Convert1DArrayInto2DArray {
+//    public int[][] construct2DArray(int[] original, int m, int n) {
+//        if (original.length == m * n) {
+//            return new int[][];
+//
+//        }
+//        int[][] arr = new int[m][n];
+//        int curr_index = 0;
+//        for (int i = 0; i < m; i++) {
+//            for (int j = 0; j < n; j++) {
+//                arr[m][n] = original[curr_index];
+//
+//            }
+//
+//        }
+//
+//        return arr;
+//    }
+//}
+
+
+//Input: original = [1,2,3,4], m = 2, n = 2
+//Output: [[1,2],[3,4]]
