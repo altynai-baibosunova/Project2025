e7bee75f5301f37fd881733e03cc3b237edc61a1
Altynai <harmoni5413@gmail.com>
2025-11-04 10:11:17 -0500

    Leetcode - 350_Intersections

--- LeetCode/A_350_Intersections.java
+++ LeetCode/A_350_Intersections.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class A_350_Intersections {
     public static int[] Inter(int[] nums1, int[] nums2){
@@ -26,7 +23,34 @@ public class A_350_Intersections {
     }
 
 
-    public int[] inter(int[] nums1, int[] nums2) {
-        return new int[]{};
+    public static int[] inter(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        int i=0, j=0, k = 0;
+        int[] temp = new int[Math.min(nums1.length, nums2.length)];
+
+        while (i< nums1.length && j< nums2.length){
+            if(nums1[i] == nums2[j]){
+             temp[k] = nums1[i];
+             k++;
+                i++;
+                j++;
+            }else if(nums1[i] > nums2[j]){
+                j++;
+            }else {
+                i++;
+            }
+        }
+
+        return Arrays.copyOfRange(temp, 0, k);
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,2,1};
+        int[] nums2 = {2,2};
+        System.out.println(Arrays.toString(inter(nums1,nums2)));
     }
 }
+//Input: nums1 = [1,2,2,1], nums2 = [2,2]
+//Output: [2,2]
