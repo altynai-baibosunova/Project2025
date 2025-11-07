d57511c39969da5b8515ac3bf1cea2ab1f944636
Altynai <harmoni5413@gmail.com>
2025-11-07 17:26:39 -0500

    Leetcode - 189_RotateArray+main

--- LeetCode/A_189_RotateArray.java
+++ LeetCode/A_189_RotateArray.java
@@ -1,13 +1,17 @@
-public class A_189_RotateArray {
-    public void rotate(int[] nums, int k) {
+import java.util.Arrays;
 
-        reverse(nums, 0, k);
+public class A_189_RotateArray {
+    public static void rotate(int[] nums, int k) {
+
+        reverse(nums, 0, nums.length-1);
+        reverse(nums, 0, k-1);
         reverse(nums, k, nums.length-1);
-        reverse(nums, 0, k);
+
 
     }
 
-    private void reverse(int[] nums, int left, int right){
+
+    private static void reverse(int[] nums, int left, int right){
 
         while(left<right){
             int temp = nums[left];
@@ -16,5 +20,14 @@ public class A_189_RotateArray {
             left++;
             right--;
         }
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+
+        rotate(nums, k); // performs rotation in-place
+        System.out.println(Arrays.toString(nums));
     }
 }
