6e203f62c55929f3a235d1f837cb299d4fb2c863
Altynai <harmoni5413@gmail.com>
2025-11-08 20:42:30 -0500

    Leetcode - 350_Intersections

--- LeetCode/A_350_Intersections.java
+++ LeetCode/A_350_Intersections.java
@@ -49,7 +49,30 @@ public class A_350_Intersections {
     public static void main(String[] args) {
         int[] nums1 = {1,2,2,1};
         int[] nums2 = {2,2};
-        System.out.println(Arrays.toString(inter(nums1,nums2)));
+        System.out.println(Arrays.toString(inters(nums1,nums2)));
+    }
+
+
+
+    public static int[] inters(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        int i=0;
+        int j=0;
+        int k=0;
+        while(i< nums1.length && j< nums2.length){
+            if(nums1[i] == nums2[j]) {
+                nums1[k++] = nums1[i];
+                i++;
+                j++;
+            }else if(nums1[i]>nums2[j]){
+                j++;
+            }else{
+                i++;
+            }
+        }
+        return Arrays.copyOfRange(nums1, 0, k);
     }
 }
 //Input: nums1 = [1,2,2,1], nums2 = [2,2]
