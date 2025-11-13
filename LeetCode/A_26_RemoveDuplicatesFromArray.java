ffc2365fd836fbd17c62803c048baa8c3106755f
Altynai <harmoni5413@gmail.com>
2025-11-12 22:55:27 -0500

    Leetcode - 26

--- LeetCode/A_26_RemoveDuplicatesFromArray.java
+++ LeetCode/A_26_RemoveDuplicatesFromArray.java
@@ -4,9 +4,9 @@ import java.util.Set;
 public class A_26_RemoveDuplicatesFromArray {
     public static int removeDuplicates(int[] nums) {
         Set<Integer> seen = new HashSet<>();
-        int k=0;
-        for (int num: nums) {
-            if(!seen.contains(num)){
+        int k = 0;
+        for (int num : nums) {
+            if (!seen.contains(num)) {
                 seen.add(num);
                 nums[k] = num;
                 k++;
@@ -16,6 +16,22 @@ public class A_26_RemoveDuplicatesFromArray {
     }
 
     public static void main(String[] args) {
-        System.out.println(removeDuplicates(new int[]{1, 2, 3, 4, 4, 5, 5, 6}));
+        System.out.println(removeDupls(new int[]{1, 2, 3, 4, 4, 5, 5, 6}));
     }
-}
+
+
+    public static int removeDupls(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        int index = 0;
+        for (int num : nums) {
+            if (!seen.contains(num)) {
+                seen.add(num);
+                nums[index] = num;
+                index++;
+            }
+        }
+            return index;
+        }
+    }
+
+
