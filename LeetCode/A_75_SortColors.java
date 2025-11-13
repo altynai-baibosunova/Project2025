0e735558efa8ba3a56e0ce7e7f0188ba3b740bd9
Altynai <harmoni5413@gmail.com>
2025-11-13 12:15:26 -0500

    Leetcode - 75+main

--- LeetCode/A_75_SortColors.java
+++ LeetCode/A_75_SortColors.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class A_75_SortColors {
-    public static void sortColors(int[] nums) {
+    public static int[] sortColors(int[] nums) {
         List<Integer> list = new ArrayList<>();
         int n = nums.length;
         for (int i = 0; i < n-1; i++) {
@@ -17,6 +18,12 @@ public class A_75_SortColors {
             }
 
         }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,3,2,0,2,2,3,1};
+        System.out.println(Arrays.toString(sortColors(nums)));
     }
 }
 //Example 1:
