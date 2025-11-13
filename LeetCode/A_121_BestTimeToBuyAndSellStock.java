e8fe7ef484ec681a862c98548461c006c423b896
Altynai <harmoni5413@gmail.com>
2025-11-12 22:22:28 -0500

    Leetcode - 121

--- LeetCode/A_121_BestTimeToBuyAndSellStock.java
+++ LeetCode/A_121_BestTimeToBuyAndSellStock.java
@@ -11,4 +11,43 @@ public class A_121_BestTimeToBuyAndSellStock {
         }
         return maxProfit;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public int maxProf(int[] prices) {
+        int profit = 0;
+        int lowest_price = prices[0];
+
+
+        for(int today_price : prices){    // 7:
+            if(today_price>lowest_price){ //
+                lowest_price = Math.min(today_price, profit);
+                profit = Math.max(today_price, profit-lowest_price);
+
+            }
+        }
+        return profit;
+    }
 }
+//Input: prices = [7,1,5,3,6,4]
+//Output: 5
+//Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+//Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
