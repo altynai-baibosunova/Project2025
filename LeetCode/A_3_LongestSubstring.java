36a150a7bf8268831ba2b11c6af59682fd7d9df2
Altynai <harmoni5413@gmail.com>
2025-11-14 14:34:03 -0500

    Leetcode - 3

---
+++ LeetCode/A_3_LongestSubstring.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class A_3_LongestSubstring {
+    public int lengthOfLongestSubstring(String s) {
+        int maxL = 0;
+        for (int start = 0; start < s.length(); start++) {
+            Set<Character> seen = new HashSet<>();
+            for (int end = start; end < s.length(); end++) {
+                char letter = s.charAt(end);
+                if(seen.contains(letter)){
+                    break;
+                }
+                seen.add(letter);
+                int currentL = end-start+1;
+                maxL = Math.max(maxL, currentL);
+            }
+
+        }
+        return maxL;
+    }
+}
