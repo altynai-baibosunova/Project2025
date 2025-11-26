c349da909ba31a5d2ee5e391c1244e58d42dc9f3
Altynai <harmoni5413@gmail.com>
2025-11-26 18:10:25 -0500

    Leetcode - 21

--- LeetCode/A_21_Merged2SortedArr.java
+++ LeetCode/A_21_Merged2SortedArr.java
@@ -191,4 +191,36 @@ public class A_21_Merged2SortedArr {
         }
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static ListNode mergeTwoL(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
+        while(list1 != null && list2 != null){
+            if(list1.val > list2.val){
+                current.next = list2;
+                list2 = list2.next;
+            }else {
+                current.next = list1;
+                list1 = list1.next;
+            }
+            current = current.next;
+        }
+        if(list1 != null) current.next = list1;
+        if (list2 != null) current.next = list2;
+        return dummy.next;
+    }
+
 }
