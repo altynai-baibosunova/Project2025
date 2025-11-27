3a260d0ffbce6f6d9441bf33fe38921d7aa1b74e
Altynai <harmoni5413@gmail.com>
2025-11-26 20:49:12 -0500

    Leetcode - 242

--- LeetCode/A_242_Anagram.java
+++ LeetCode/A_242_Anagram.java
@@ -16,16 +16,13 @@ public class A_242_Anagram {
         }
 
     public static void main(String[] args) {
-            A_242_Anagram obj = new A_242_Anagram();
+
             String s = "Mama";
             String t = "Papa";
-           boolean result = obj.isAnagram(s,t);
 
-        System.out.println(isA("rat", "atr"));
+
+        System.out.println(isAnagramACSII(s,t));
     }
-
-
-
 
 
     public static boolean isAnagr(String s, String t) {
@@ -55,6 +52,25 @@ public class A_242_Anagram {
                 if (n != 0) return false;
             }
             return true;
+    }
+
+    public static boolean isAnagramACSII(String s, String t) {
+            int[] count = new int[26];
+
+            for(int i=0; i<s.length(); i++){
+                char sa = s.charAt(i);
+                count[sa-'a']++;
+            }
+        for(int i=0; i<t.length(); i++){
+            char ta = t.charAt(i);
+            count[ta-'a']--;
+        }
+        for (int i : count){
+            if(i != 0){
+                return false;
+            }
+        }
+        return true;
     }
     }
 
