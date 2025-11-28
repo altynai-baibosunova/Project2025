c3ca7b5847fa064de309e70cbd46c2657a904f75
Altynai <harmoni5413@gmail.com>
2025-11-28 13:47:31 -0500

    Leetcode - 22

--- LeetCode/A_22_GenerateParentese.java
+++ LeetCode/A_22_GenerateParentese.java
@@ -19,6 +19,55 @@ public class A_22_GenerateParentese {
     }
 
     public static void main(String[] args) {
-        System.out.println(generateParenthesis(3));
+        System.out.println(generatePar(3));
     }
+
+
+    public static List<String> generatePar(int n) {
+        List<String> result = new ArrayList<>();
+        back(result,0,0,"", n);
+        return result;
+    }
+
+    private static void back(List<String> result, int start, int end, String s, int n){
+        if(s.length() == 2*n){
+            result.add(s);
+        }if(start<n){
+            back(result,start+1, end, s+"(", n);
+        }if(end<start){
+            back(result, start, end+1, s+")", n);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
