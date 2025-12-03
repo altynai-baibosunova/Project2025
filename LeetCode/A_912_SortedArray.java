93f087f652f9a6471856d0164fab7aecafcbe670
Altynai <harmoni5413@gmail.com>
2025-12-02 21:55:16 -0500

    Leetcode - 912

--- LeetCode/A_912_SortedArray.java
+++ LeetCode/A_912_SortedArray.java
@@ -74,7 +74,26 @@ public class A_912_SortedArray {
 
     public static void main(String[] args) {
         int[] nums = {5,2,3,1};
-        System.out.println(Arrays.toString(sortAr(nums)));
+        System.out.println(Arrays.toString(sort(nums)));
+    }
+
+
+
+
+    public static int[] sort(int[] nums) {
+        int n = nums.length;
+        for(int i=0; i<n-1; i++){
+            for(int j=0; j<n-i-1; j++){
+                if(nums[j] > nums[j+1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+            }
+        }
+
+return nums;
+
     }
         }
 
