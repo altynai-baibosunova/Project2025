62a29dec79d9b2f443ce888ce37464d5e6b28d79
Altynai <harmoni5413@gmail.com>
2025-12-06 14:12:29 -0500

    Leetcode - 167+main

--- LeetCode/A_167_TwoSum_InputArray.java
+++ LeetCode/A_167_TwoSum_InputArray.java
@@ -1,12 +1,19 @@
+import java.util.Arrays;
+
 public class A_167_TwoSum_InputArray {
-    public int[] twoSum(int[] numbers, int target) {
+    public static int[] twoSum(int[] numbers, int target) {
         int left=0;
         int right= numbers.length-1;
 
         while (numbers[left]+numbers[right] != target){
-            if(numbers[left]+numbers[right] != target) left++;
+            if(numbers[left]+numbers[right] < target) left++;
             else right--;
         }
         return new int[]{left+1,right+1};
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {2,7,11,15};
+        System.out.println(Arrays.toString(twoSum(numbers,9)));
     }
 }
