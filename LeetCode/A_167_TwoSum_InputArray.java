393d9fe33542d119b2618cbe8a6d03bc374183c7
Altynai <harmoni5413@gmail.com>
2025-12-06 14:09:58 -0500

    Leetcode - 167

---
+++ LeetCode/A_167_TwoSum_InputArray.java
@@ -0,0 +1,12 @@
+public class A_167_TwoSum_InputArray {
+    public int[] twoSum(int[] numbers, int target) {
+        int left=0;
+        int right= numbers.length-1;
+
+        while (numbers[left]+numbers[right] != target){
+            if(numbers[left]+numbers[right] != target) left++;
+            else right--;
+        }
+        return new int[]{left+1,right+1};
+    }
+}
