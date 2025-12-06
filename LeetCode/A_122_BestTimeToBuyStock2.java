b3ec40c55ae346f7c01c8f551f157aec30720b5f
Altynai <harmoni5413@gmail.com>
2025-12-05 23:20:29 -0500

    Leetcode - 122

--- LeetCode/A_122_BestTimeToBuyStock2.java
+++ LeetCode/A_122_BestTimeToBuyStock2.java
@@ -30,14 +30,35 @@ public class A_122_BestTimeToBuyStock2 {
     public static int maxPr(int[] prices) {
         int profit = 0;
 
-        for(int i=1; i<prices.length; i++){
-            if(prices[i]>prices[i-1]){
-                profit += prices[i]-prices[i-1];
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
             }
         }
         return profit;
+
+
     }
-}
+
+
+            public static int mat(int[] prices) {
+
+                int index = 0;
+
+                for(int i=0; i<prices.length-1; i++){
+                    int profit = 0;
+                    if(prices[i]>prices[i+1]){ // i= 7  1  5. 3. 6. 4
+                        //i-1=.  1  5  3. 6. 4
+                        i++;
+                    } else{
+                        profit = prices[i]-prices[i-1];
+                        prices[index++] = prices[i];
+                    }
+                }
+                return index;
+            }
+        }
+
 
 
 
